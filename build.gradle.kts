e041dd2367a29bb96925ab0c9b0b595139292c0f
Juanes191 <juanesvilla2016@gmail.com>
2023-11-15 11:15:26 -0500

    F Commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.17"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.unisabana"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-amqp")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.amqp:spring-rabbit-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	builder = "paketobuildpacks/builder-jammy-base:latest"
+}
---
+++ src/main/java/com/unisabana/rabbitmqproject/RabbitmqprojectApplication.java
@@ -0,0 +1,13 @@
+package com.unisabana.rabbitmqproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqprojectApplication.class, args);
+	}
+
+}
